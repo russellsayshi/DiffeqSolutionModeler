08f07dd8f2aa0aac5fa8b51fcb21afd3e3a76514
Russell Coleman <russellsayshi@gmail.com>
2018-11-26 17:14:14 -0600

    moved classes to src

---
+++ src/Vector.java
@@ -0,0 +1,22 @@
+/**
+ * This class is just a matrix
+ * with only one column.
+ */
+
+public class Vector extends Matrix {
+	public Vector(int rows) {
+		super(rows, 1);
+	}
+
+	public int dimension() {
+		return super.rows();
+	}
+
+	public double get(int row) {
+		return super.get(row, 1);
+	}
+
+	public void set(int row, double val) {
+		super.set(row, 1, val);
+	}
+}
